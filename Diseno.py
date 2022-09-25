2c549c61e92cc8da0a514d4eb40eb4d11dd084f3
JulianVillad <julian.villad@udea.edu.co>
2022-09-25 14:31:49 -0500

    "Version preliminar"

--- Diseno.py
+++ Diseno.py
@@ -8,37 +8,38 @@ from PyQt5.QtWidgets import (QApplication, QDialog, QMainWindow,
 import sqlite3 as sql                                               #Sqlite3 para la base de datos
 from bdatos import BaseDatos                                        #Importamos la clase para administrar la base de datos
 
-class ventanappal(QMainWindow):
+class ventanappal(QMainWindow):                                     #Programa de reservas
     def __init__(self):
         super(ventanappal, self).__init__()
         loadUi('Interfaz.ui',self)
 
+        #Botones de navegación (Barra superior):
         self.btn_inicio.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pg_inicio))
         self.btn_tarifas.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pg_tarifas))
         self.btn_modificar.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pg_modificar))
         self.btn_admin.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.pg_admin))
         self.btn_salir.clicked.connect(lambda: self.close())
 
-        self.BBDD = BaseDatos()
+        self.BBDD = BaseDatos()                                     #Llamo la función creada en bdatos.py
+        
+        self.btn_consultar_mod.clicked.connect(self.buscarevento)       #Botón buscar evento
+        self.btn_enviar_ini.clicked.connect(self.reservaeventos)        #Botón reservar
+        self.btn_refresh_leven.clicked.connect(self.consultaevento)     #Botón actualizar
+        self.btn_cancelar_mod.clicked.connect(self.cancelarevento)      #Botón cancelar evento
+        self.btn_modificar_mod.clicked.connect(self.modificarevento)    #Botón modificar evento
+        self.btn_ordenar_leven.clicked.connect(self.ordenartabla)       #Botón ordenar por fecha
 
-        #self.btn_dispon_ini.clicked.connect(self.consultabd)
-        self.btn_enviar_ini.clicked.connect(self.reservaeventos)
-        self.btn_refresh_leven.clicked.connect(self.consultaevento)
-        #self.btn_cancelar_ini_clicked.connect(self.close)
-
-    def consultaevento(self):
+    def consultaevento(self):                                       #Consulta de eventos
         datos = self.BBDD.consultabd()
         i = len(datos)
-        print(datos)
+        #print(datos)
         self.tableW.setRowCount(i)
-        print(i)
-        tablerow = 1
-        print(datos[0])
-        self.tableW.setItem(tablerow, 0, row[0])
+        tablerow = 0
+
         for row in datos:
             print(row[0])
-            self.tableW.setItem(tablerow, 0, row[0])
-            self.tableW.setItem(tablerow, 1, row[1])
+            self.tableW.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+            self.tableW.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row[1]))
             self.tableW.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[2]))
             self.tableW.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(row[3]))
             self.tableW.setItem(tablerow, 4, QtWidgets.QTableWidgetItem(row[4]))
@@ -47,33 +48,77 @@ class ventanappal(QMainWindow):
             self.tableW.setItem(tablerow, 7, QtWidgets.QTableWidgetItem(row[7]))
             tablerow+=1
 
-    def reservaeventos(self):
-        ID1 = self.LE_ID_ini.int()
-        Nombre1 = self.LE_Nombre_ini().text()
-        Apellido1 = self.LE_Apellidos_ini().text()
-        Celular1 = self.LE_Cel_ini().text()
-        Correo1 = self.LE_Correo_ini().text()
-        Salon1 = self.cBsalon_ini.text()
-        fechai = self.clicked.connect(self.calendarW_ini.selectedDate())
-        print(self.calendarW_ini.selectedDate)
-        fechait = str(fechai)
-        self.LE_finicial_ini.setText(fechait)
-        Fecha_inicial1 = self.LE_finicial_ini().text()
-        Fecha_final1 = self.LE_Ffinal_ini().text()
+    def reservaeventos(self):                                       #Registrar un evento
+        ID1 = self.LE_ID_ini.text()
+        Nombre1 = self.LE_Nombre_ini.text().upper()
+        Apellido1 = self.LE_Apellidos_ini.text().upper()
+        Celular1 = self.LE_Cel_ini.text()
+        Correo1 = self.LE_Correo_ini.text().upper()
+        Salon1 = self.cBsalon_ini.currentText()
+        Fecha_inicial1 = self.LE_finicial_ini.text()
+        Fecha_final1 = self.LE_ffinal_ini.text()
         if ID1 != '' and Nombre1 != '' and Apellido1 != '' and Celular1 != '' and Salon1 != '' and Fecha_inicial1 != '' and Fecha_final1 != '':
             self.BBDD.reservabd(ID1,Nombre1,Apellido1,Celular1,Correo1,Salon1,Fecha_inicial1,Fecha_final1)
             self.lbl_confirm_ini.setText('Registro exitoso')
             self.LE_ID_ini.clear()
-            self.LE_Nombre_ini().clear()
-            self.LE_Apellidos_ini().clear()
-            self.LE_Cel_ini().clear()
+            self.LE_Nombre_ini.clear()
+            self.LE_Apellidos_ini.clear()
+            self.LE_Cel_ini.clear()
             self.LE_Correo_ini.clear()
             self.LE_finicial_ini.clear()
             self.LE_ffinal_ini.clear()
         else:
             self.lbl_confirm_ini.setText('Por favor llene todos los campos')
 
-
+    def buscarevento(self):                                         #Consultar un evento
+        Fcan = str(self.LE_finicial_mod.text())
+        Scan = str(self.cB_salon_mod.currentText())
+        evento = self.BBDD.buscarevento(Fcan, Scan)
+        #print(evento)
+        if len(evento) == 0:
+            self.lbl_alerta_mod.setText('No existen eventos para esas fechas')
+            self.LE_ID_mod.setText('')
+            self.LE_ffinal_mod.setText('')
+            self.LE_nombre_mod.setText('')
+            self.LE_apellido_mod.setText('')
+            self.LE_cel_mod.setText('')
+            self.LE_correo_mod.setText('')
+        else:
+            self.LE_ID_mod.setText(evento[0][0])
+            self.LE_ffinal_mod.setText(evento[0][7])
+            self.LE_nombre_mod.setText(evento[0][1])
+            self.LE_apellido_mod.setText(evento[0][2])
+            self.LE_cel_mod.setText(evento[0][3])
+            self.LE_correo_mod.setText(evento[0][4])
+    
+    def cancelarevento(self):                                       #Cancelar un evento
+        Fcan = str(self.LE_finicial_mod.text())
+        Scan = str(self.cB_salon_mod.currentText())
+        evento = self.BBDD.buscarevento(Fcan, Scan)
+        #print(evento)
+        if len(evento) != 0:
+            self.BBDD.cancelarev(Fcan, Scan)
+        
+    def modificarevento(self):                                      #Modificar un evento
+        Fcan = str(self.LE_finicial_mod.text())
+        Scan = str(self.cB_salon_mod.currentText())
+        evento = self.BBDD.buscarevento(Fcan, Scan)
+        #print(evento)
+        if len(evento) != 0:
+            IDmod = self.LE_ID_mod.text()
+            nwnombre = self.LE_nombre_mod.text()
+            nwapellido = self.LE_apellido_mod.text()
+            nwcel = self.LE_cel_mod.text()
+            nwcorreo = self.LE_correo_mod.text()
+            nwsalon = self.cB_salon_mod.currentText()
+            nwfinicial = self.LE_finicial_mod.text()
+            nwffinal = self.LE_ffinal_mod.text()
+            self.BBDD.modificarevento(nwnombre, nwapellido, nwcel, nwcorreo, nwsalon, nwfinicial, nwffinal,IDmod)
+    
+    def ordenartabla(self):
+        self.BBDD.orden()
+        
+        
 if __name__ == "__main__":              #Evita que se ejecuten los comandos, solo las funciones definidas
     app = QApplication(sys.argv)
     mi_app = ventanappal()
--- bdatos.py
+++ bdatos.py
@@ -6,7 +6,7 @@ class BaseDatos():
 
     def reservabd(self,ID,Nombre,Apellidos,Celular,Correo,Salon,Fecha_inicial,Fecha_final):
         cursor = self.conexion.cursor()
-        resbd = f'''INSERT INTO Reservas VALUES({ID},'{Nombre}','{Apellidos}',{Celular},'{Correo}',{Salon},
+        resbd = f'''INSERT INTO Reservas VALUES('{ID}','{Nombre}','{Apellidos}','{Celular}','{Correo}','{Salon}',
         '{Fecha_inicial}','{Fecha_final}')'''.format(ID,Nombre,Apellidos,Celular,Correo,Salon,Fecha_inicial,Fecha_final)
         cursor.execute(resbd)
         self.conexion.commit()
@@ -16,15 +16,44 @@ class BaseDatos():
         consbd = f'''SELECT * FROM Reservas'''
         cursor.execute(consbd)
         consulta = cursor.fetchall()
+        cursor.close()
+        print(consulta)
         return consulta
-    def consultaorden(self,campo):
+    def orden(self):
         cursor = self.conexion.cursor()
-        consbd = f'''SELECT * FROM Reservas ORDER BY {field}'''
+        consbd = f'''SELECT * FROM Reservas ORDER BY Fecha_inicial'''
         cursor.execute(consbd)
         consulta = cursor.fetchall()
+        self.conexion.commit()
+        cursor.close()
+    def cancelarev(self,Fecha_inicial,Salon):
+        cursor = self.conexion.cursor()
+        bd = f'''DELETE FROM Reservas WHERE Fecha_inicial = '{Fecha_inicial}' and Salon = '{Salon}' '''.format(Fecha_inicial, Salon)
+        cursor.execute(bd)
+        self.conexion.commit()
+        cursor.close()
+    def buscarevento(self,Fecha_inicial,Salon):
+        cursor = self.conexion.cursor()
+        consbd = f'''SELECT * FROM Reservas WHERE Fecha_inicial = '{Fecha_inicial}' and Salon = '{Salon}' '''.format(Fecha_inicial, Salon)
+        cursor.execute(consbd)
+        consulta = cursor.fetchall()
+        cursor.close()
         return consulta
-
+    def modificarevento(self,Nombre,Apellidos,Celular,Correo,Salon,Fecha_inicial,Fecha_final,ID):
+        cursor = self.conexion.cursor()
+        bd = f'''UPDATE Reservas SET Nombre ='{Nombre}', Apellido = '{Apellidos}',Celular = '{Celular}',
+        Correo = '{Correo}', Salon = '{Salon}', Fecha_inicial = '{Fecha_inicial}', 
+        Fecha_final = '{Fecha_final}' WHERE ID = '{ID}' '''.format(Nombre,Apellidos,Celular,Correo,Salon,Fecha_inicial,Fecha_final,ID)
+        cursor.execute(bd)
+        a = cursor.rowcount
+        self.conexion.commit()
+        cursor.close()
+        return a
+        
 if __name__ == "__main__":
     inst = BaseDatos()
-    #inst.reservabd(39582645,"Maria","Palacios",3125796351,"mpal@correo.co",2,"30/09/2022","31/09/2022")
-    inst.consultabd()
+    #inst.reservabd("6168148","Gregorio","Montes","654651","Grego@correo.co","2","25/09/2022","26/09/2022")
+    #inst.consultabd()
+    #inst.cancelarev("01/12/2022", "1")
+    #inst.buscarevento('10/12/2022', '1')
+    #inst.modificarevento("Marco","Marquez","654651","GREGORIO@correo.co","2","25/09/2022","26/09/2022")
